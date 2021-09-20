8b190d10945904de6de3e8a335ca4f88aa1829fd
Amospelumi <oluwapelumiabiodun45@gmail.com>
2021-09-20 12:24:24 +0100

    last update

--- Script.js
+++ Script.js
@@ -6,22 +6,34 @@
 // console.log(count);
 
 let count = 0
+let increase = document.getElementById('count-el')
+let decrease = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el')
+
 // increment function
 
 function increment(){
-    document.getElementById('count-el').innerText = count+=1
+    increase.innerText = count+=1
 }
+
+
+//  decrement function
 
 function decrement(){
     if (count <= 1){
-        count = document.getElementById('count-el').innerText = 0
+        count = decrease.innerText = 0
         return count
     }else{
-        document.getElementById('count-el').innerText = count -= 1
+        decrease.innerText = count -= 1
     }
 }
 
+// save function
+
 function save(){
-    console.log(count)
+    saveEl.innerText += count + " -"
+    increase.innerText = 0
+    count = 0
+       
 }
 
